31c6384e38e38de9c6d2307c1f56b8be4e85df63
soumakk <soumakkdutta@gmail.com>
2025-03-13 09:00:49 +0530

    cloudflare pages setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+}
 
-export default nextConfig;
+if (process.env.NODE_ENV === 'development') {
+	await setupDevPlatform()
+}
+
+export default nextConfig
