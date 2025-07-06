403b57e80cb76301ee35429dd27c0fd02e016cfa
soumakk <soumakkdutta@gmail.com>
2025-07-06 17:08:42 +0530

    project details update

--- src/components/home/Projects.utils.tsx
+++ src/components/home/Projects.utils.tsx
@@ -19,19 +19,11 @@ export interface IProject {
 
 export const projects: IProject[] = [
 	{
-		name: 'Tasks Flow',
-		description: "It's is a task management application",
-		github: 'https://github.com/soumakk/tasksflow',
-		site: 'https://tasks-flow-app.netlify.app/',
-		image: '/images/projects/task.png',
-		techstack: [Tech.React, Tech.Vite, Tech.TypeScript, Tech.Tailwind],
-	},
-	{
-		name: 'Nova Calendar',
-		description: 'A simple calendar app to track events, inspired by Google calender',
-		github: 'https://github.com/soumakk/calendar-app',
-		site: 'https://nova-calendar.netlify.app/',
-		image: '/images/projects/calendar.png',
+		name: 'Flow',
+		description: 'A simple and secure task management application',
+		github: 'https://github.com/soumakk/flow',
+		site: 'https://flow-task.netlify.app/',
+		image: '/images/projects/flow.png',
 		techstack: [Tech.React, Tech.Vite, Tech.TypeScript, Tech.Tailwind],
 	},
 	{
@@ -40,6 +32,14 @@ export const projects: IProject[] = [
 		github: 'https://github.com/soumakk/shader-editor',
 		site: 'https://shader-editor.netlify.app/',
 		image: '/images/projects/editor.png',
+		techstack: [Tech.React, Tech.Vite, Tech.TypeScript, Tech.Tailwind],
+	},
+	{
+		name: 'Nova Calendar',
+		description: 'A simple calendar app to track events, inspired by Google calender',
+		github: 'https://github.com/soumakk/calendar-app',
+		site: 'https://nova-calendar.netlify.app/',
+		image: '/images/projects/calendar.png',
 		techstack: [Tech.React, Tech.Vite, Tech.TypeScript, Tech.Tailwind],
 	},
 	{
